ff307a44d4cd694d6b68b0dc23a2011e97303305
Mahmoud Sayed <138359247+Mahmovdsayed@users.noreply.github.com>
2024-07-30 04:37:11 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,70 +1,10 @@
 const express = require("express");
 const cors = require("cors");
-const { tikdown } = require("nayan-media-downloader");
-
+const { downloadVideo } = require("tiktok-api-downloader");
 const app = express();
 const PORT = process.env.PORT || 4000;
 
 app.use(cors());
-
-const fetchVideoData = async (url) => {
-  const options = {
-    method: "POST",
-    headers: {
-      accept: "application/json",
-    },
-  };
-
-  const response = await fetch(
-    `https://snapdouyin.app/wp-json/aio-dl/video-data/?url=${url}`,
-    options
-  );
-  return response.json();
-};
-
-const formatResponse = (downloadURL, data) => {
-  const {
-    view: likes = null,
-    play: plays = null,
-    comment = null,
-    share = null,
-    download: downloadCount = null,
-    duration: durationInSeconds = null,
-    title = null,
-    author = {},
-  } = downloadURL.data;
-
-  const {
-    id = null,
-    unique_id: uniqueId = null,
-    nickname = null,
-    avatar = null,
-  } = author;
-
-  return {
-    status: true,
-    developer: "API BY MAHMOUD SAYED || https://github.com/Mahmovdsayed",
-    metadata: {
-      likes,
-      plays,
-      comment,
-      share,
-      downloadCount,
-      durationInSeconds,
-      title,
-      thumbnail: data.thumbnail || null,
-      url: data.url || null,
-    },
-    author: {
-      id,
-      unique_id: uniqueId,
-      nickname,
-      avatar,
-      profileURL: uniqueId ? `https://www.tiktok.com/@${uniqueId}` : null,
-    },
-    download: data.medias,
-  };
-};
 
 app.get("/", (req, res) => {
   return res.status(200).json({
@@ -82,10 +22,7 @@ app.get("/download", async (req, res) => {
   }
 
   try {
-    const videoData = await fetchVideoData(url);
-    const downloadURL = await tikdown(url);
-
-    const response = formatResponse(downloadURL, videoData);
+    let response = await downloadVideo(url);
     return res.status(200).json(response);
   } catch (error) {
     console.error(error);
