8daa6f7931c91fa5bf791670d3d633db0874c6bf
Mahmoud Sayed <138359247+Mahmovdsayed@users.noreply.github.com>
2024-07-30 04:41:45 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const express = require("express");
-const cors = require("cors");
-const { downloadVideo } = require("tiktok-api-downloader");
+import express from "express";
+import cors from "cors";
+import { downloadVideo } from "tiktok-api-downloader";
 const app = express();
 const PORT = process.env.PORT || 4000;
 
